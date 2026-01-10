679ab263bc9d24c3f05523f53c9464be871966c0
Vivian <vivianprofissi7800@gmail.com>
2026-01-10 11:10:41 -0300

    back

--- src/main/java/org/com/programming/animal/controller/AuthController.java
+++ src/main/java/org/com/programming/animal/controller/AuthController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("auth")
+/* TESTE */
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
