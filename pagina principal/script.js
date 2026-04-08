0a43fa98c063378f2d22ea4988384102a14fe70c
Vivian <vivianprofissi7800@gmail.com>
2026-04-08 20:46:10 -0300

    Tela principal em andamento .V1

---
+++ pagina principal/script.js
@@ -0,0 +1,15 @@
+
+// FAZ O MENU DO USUÁRIO 
+function toggleMenu() {
+    const menu = document.getElementById("user_menu");
+    menu.classList.toggle("hidden");
+}
+
+window.onclick = function(event) {
+    if (!event.target.closest('.user-container')) {
+        const menu = document.getElementById("user_menu");
+        if (!menu.classList.contains('hidden')) {
+            menu.classList.add('hidden');
+        }
+    }
+}
